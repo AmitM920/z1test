88c6aaf40a16e7fdedde3aa1484933f4f2807040
AmitM920 <138188056+AmitM920@users.noreply.github.com>
2025-03-24 18:55:56 +0530

    Your commit message here

--- Component/Comp1.jsx
+++ Component/Comp1.jsx
@@ -1,15 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function Comp1() {
-    const [Data,setData]=useState([])
-    useEffect(()=>{
-    
-    },[])
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    // Fetch 10 cat images from The Cat API
+    axios.get("https://api.thecatapi.com/v1/images/search?limit=10")
+      .then(res => {
+        setCats(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
+  const list = cats.map(cat => (
+    <tr key={cat.id}>
+      <td>
+        <img src={cat.url} alt="Cat" width="200" />
+      </td>
+    </tr>
+  ));
+
   return (
     <div>
-
+      <table border="1" width="100%">
+        <thead>
+          <tr>
+            <th>Cat Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default Comp1
+export default Comp1;
