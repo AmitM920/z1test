7d1b63445cb0a9fee0c707ec3b08c82d75789bc0
AmitM920 <138188056+AmitM920@users.noreply.github.com>
2025-03-24 18:45:05 +0530

    Initial commit

---
+++ Component/Comp1.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+function Comp1() {
+    const [Data,setData]=useState([])
+    useEffect(()=>{
+    
+    },[])
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Comp1
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Comp1 from '../Component/Comp1'
+
+function App() {
+  return (
+    <div>
+
+      <Comp1></Comp1>
+    </div>
+  )
+}
+
+export default App
